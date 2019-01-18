4d08b9a021662da8c569bd27599c8390851e9ab3
Mikronexus <mikronexus@Mikronexuss-Mac-mini.local>
2019-01-18 18:45:32 +0500

    df

--- src/app/global.ts
+++ src/app/global.ts
@@ -119,7 +119,7 @@ export class GlobalVariable {
     //
     reward_menu_length:any = 0;
     constructor(public alertCtrl: AlertController,public toastCtrl: ToastController) {
-     this.BaseUrl = 'https://staging.onlineordering.mikronexus.com/index.php/';
+     this.BaseUrl = 'https://onlineordering.mikronexus.com/online-ordering-new/index.php/';
     }
     
 
