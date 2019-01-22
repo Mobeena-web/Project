76d22c1d89abd823e6756004c8011e11459e534d
Mikronexus <mikronexus@Mikronexuss-Mac-mini.local>
2019-01-22 12:30:06 +0500

    new

--- src/app/global.ts
+++ src/app/global.ts
@@ -107,7 +107,7 @@ export class GlobalVariable {
     is_birthday:any;
     is_anniversary:any;
     order_instructions:any;
-    caos_flag:boolean = false;
+    caos_flag:boolean = true;
     caos_udid = 'e2c40e9f0a';
     welcome:any;
     inradius:boolean = true;
