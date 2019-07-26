fd7d8a86ab210f3b1d3e78c45a603acf926fb951
Mac os <ayubbscs@gmail.com>
2019-07-26 12:50:10 +0500

    updates by Ayub

--- src/app/global.ts
+++ src/app/global.ts
@@ -133,9 +133,9 @@ export class GlobalVariable {
     business_type:any;
     orders_enabled:any;
     constructor(public alertCtrl: AlertController,public toastCtrl: ToastController) {
-        //this.BaseUrl = 'https://staging.onlineordering.mikronexus.com/index.php/';
-    //  this.BaseUrl = 'https://onlineordering.mikronexus.com/online-ordering-new/index.php/';
-        this.BaseUrl = 'http://192.168.100.3/online-ordering-rewamp/index.php/'
+        // this.BaseUrl = 'https://staging.onlineordering.mikronexus.com/index.php/';
+     this.BaseUrl = 'https://onlineordering.mikronexus.com/online-ordering-new/index.php/';
+        // this.BaseUrl = 'http://192.168.100.3/online-ordering-rewamp/index.php/'
 }
     
 
