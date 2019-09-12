1ead829d18ca16d2928c6e85ec32668854ae9223
Mac os <ayubbscs@gmail.com>
2019-09-12 17:27:17 +0500

    updates

--- src/app/global.ts
+++ src/app/global.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core'
-import { AlertController  } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 import { ToastController } from 'ionic-angular';
-import {CONFIG} from '../../app-config';
+import { CONFIG } from '../../app-config';
 declare var Circles;
 
 
 @Injectable()
 export class GlobalVariable {
-    staging: boolean = false; 
+    staging: boolean = false;
     //Product: Array<{ productName: string, image: string, Quantity: number,price: number  }> = [];
     cartflag: boolean = true;
     showFabFlag: boolean;
@@ -30,7 +30,7 @@ export class GlobalVariable {
     RadeemUrl: string;
     minimun_order: any;
     pickup_Time: any;
-    savePickupTime : any ;
+    savePickupTime: any;
     deliveryCharges: any = 0;
     HomeFlag: boolean;
     WallletLock: boolean = false;
@@ -54,7 +54,7 @@ export class GlobalVariable {
     points_availed: any = 0;
     reservation_id: any;
     type: any;
-    OrderType : any;
+    OrderType: any;
     username: any;
     punchCount: any = 0;
     RewardCount: any = 0;
@@ -63,83 +63,83 @@ export class GlobalVariable {
     availed_discount_count: any = 0;
     business_discount_count: any = 0;
     TotalbadgeValue: any = 0;
-    MinValue : any ;
-    DeliveryTime : any ;
-    estimated_time : any;
-    itemInstruction : any ;
-    RewardsPos : any ; 
-    Email : any;
-    PhoneNo: any ;
-    phone_verified :boolean;
-    email_verified :boolean;
+    MinValue: any;
+    DeliveryTime: any;
+    estimated_time: any;
+    itemInstruction: any;
+    RewardsPos: any;
+    Email: any;
+    PhoneNo: any;
+    phone_verified: boolean;
+    email_verified: boolean;
     business_username = CONFIG.business_username;;
-    jackemial:any;
-    new_id=CONFIG.business_id;
-    model_flag :boolean = true;
-    signupData:any;
-    percent:any;
-    mycoordinates:any;
-    save_check:boolean = false;
-    points_:any;
-    cash_enabled:boolean;
-    special_offer:any;
-    events_enabled:any;
-    firstName:any;
-    lastName:any;
-    gallery_enabled:any;
-    android_url:any;
-    ios_url:any;
-    update_message:any;
+    jackemial: any;
+    new_id = CONFIG.business_id;
+    model_flag: boolean = true;
+    signupData: any;
+    percent: any;
+    mycoordinates: any;
+    save_check: boolean = false;
+    points_: any;
+    cash_enabled: boolean;
+    special_offer: any;
+    events_enabled: any;
+    firstName: any;
+    lastName: any;
+    gallery_enabled: any;
+    android_url: any;
+    ios_url: any;
+    update_message: any;
     app_version = "2.1";
-    notifications:any;
-    guess_login:boolean = false;
-    banner_image:any;
-    punch_:any;
-    punch_limit_:any;
-    latitude:any;
-    longitude:any;
+    notifications: any;
+    guess_login: boolean = false;
+    banner_image: any;
+    punch_: any;
+    punch_limit_: any;
+    latitude: any;
+    longitude: any;
     hours_operation;
-    welcome_data:any;
-    branch_enabled:any;
-    giftcard_enabled:any;
-    business_list:any;
-    b_logo:any;
-    is_birthday:any;
-    is_anniversary:any;
-    order_instructions:any;
-    caos_flag:boolean = false;
+    welcome_data: any;
+    branch_enabled: any;
+    giftcard_enabled: any;
+    business_list: any;
+    b_logo: any;
+    is_birthday: any;
+    is_anniversary: any;
+    order_instructions: any;
+    caos_flag: boolean = false;
     caos_udid = 'e2c40e9f0a';
-    welcome:any;
-    inradius:boolean = true;
-    pickup_timing:any;
-    delivery_timing:any;
+    welcome: any;
+    inradius: boolean = true;
+    pickup_timing: any;
+    delivery_timing: any;
     // punch - point variable
     punch_check: any;
     point_check: any;
     //
-    reward_menu_length:any = 0;
-    specific_delivery_day:any;
-    delivery_day:any;
-    review_enabled:any;
+    reward_menu_length: any = 0;
+    specific_delivery_day: any;
+    delivery_day: any;
+    review_enabled: any;
     menu_item_id: any;
-    menu_item_arr :any =[];
-    
-    admin_stripe_enabled:boolean = false;
-    authorize_enabled:boolean = false;
-    card_enabled:boolean = false;
+    menu_item_arr: any = [];
+
+    admin_stripe_enabled: boolean = false;
+    authorize_enabled: boolean = false;
+    card_enabled: boolean = false;
     catering_enabled = false;
     catering_cart_enabled = false;
-    giftcard_amount_limit:any = 0;
-    schedule_day_id:any;
-    schedule_converted_time:any;
-    business_type:any;
-    orders_enabled:any;
-    constructor(public alertCtrl: AlertController,public toastCtrl: ToastController) {
+    giftcard_amount_limit: any = 0;
+    schedule_day_id: any;
+    schedule_converted_time: any;
+    business_type: any;
+    orders_enabled: any;
+    constructor(public alertCtrl: AlertController, public toastCtrl: ToastController) {
         // this.BaseUrl = 'https://staging.onlineordering.mikronexus.com/index.php/';
-     this.BaseUrl = 'https://onlineordering.mikronexus.com/online-ordering-new/index.php/';
+        this.BaseUrl = 'https://onlineordering.mikronexus.com/online-ordering-new/index.php/';
         // this.BaseUrl = 'http://192.168.100.3/online-ordering-rewamp/index.php/'
-}
-    
+    }
+
 
     alertMessage(title, subTitle) {
         let alert = this.alertCtrl.create({
@@ -149,34 +149,34 @@ export class GlobalVariable {
         });
         alert.present();
     }
-     presentToast(msg) {
+
+    presentToast(msg) {
         let toast = this.toastCtrl.create({
-        message: msg,
-        duration: 3000
+            message: msg,
+            duration: 3000
         });
         toast.present();
     }
 
-    circle_graph(value,id,radius,width,color){
-       
+    circle_graph(value, id, radius, width, color) {
         var myCircle = Circles.create({
-            id:                  id,
-            radius:              radius,
-            value:               value,
-            maxValue:            100,
-            width:               width,
-            text:                function(value){return '';},
-            colors:              [color, '#273C75'],
-            duration:            400,
-            wrpClass:            'circles-wrp',
-            textClass:           'circles-text',
-            valueStrokeClass:    'circles-valueStroke',
+            id: id,
+            radius: radius,
+            value: value,
+            maxValue: 100,
+            width: width,
+            text: function (value) { return ''; },
+            colors: [color, '#273C75'],
+            duration: 400,
+            wrpClass: 'circles-wrp',
+            textClass: 'circles-text',
+            valueStrokeClass: 'circles-valueStroke',
             maxValueStrokeClass: 'circles-maxValueStroke',
-            styleWrapper:        true,
-            styleText:           true
-          });
+            styleWrapper: true,
+            styleText: true
+        });
     }
-   
+
 }
 
 
