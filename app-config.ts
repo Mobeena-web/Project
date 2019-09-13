30dcd056c92ae1d66cc621802a3c1b8736298436
Malik Jahanzaib <malikjahanzaibsohail@gmail.com>
2019-09-13 17:47:51 +0500

    -

--- app-config.ts
+++ app-config.ts
@@ -1,7 +1,1 @@
-export let CONFIG = { 
-    business_username: "mizuki",
-    business_id: "118",
-    appId: 'd55e8d8a-15b9-4529-bd56-eb65dce5bffb',
-    googleProjectId:  '773485528357',
-    marketPlace:false
-};
+export let CONFIG = { business_username: "mknx", business_id: 76, appId: "19271927", googleProjectId: "87381638126"};
