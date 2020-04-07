d50f41aec6642cacdf40765b8adafdcbb08e2ee2
MacBook Pro Ratina <macbookproratina@MacBooks-MacBook-Pro.local>
2020-04-07 22:35:37 +0500

    ---

--- app-config.ts
+++ app-config.ts
@@ -1,1 +1,1 @@
-export let CONFIG = { business_username: "glensdinette", business_id: "97", appId: "0", googleProjectId: "0", marketPlace: false};
+export let CONFIG = { business_username: "greekrave", business_id: "170", appId: "0", googleProjectId: "0", marketPlace: false};
