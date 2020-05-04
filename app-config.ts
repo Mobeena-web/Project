2ebcb0077a14e46d370ef44857d9496de69c5c47
MacBook Pro Ratina <macbookproratina@MacBooks-MacBook-Pro.local>
2020-05-04 12:27:27 +0500

    keyboard and cordova webview updated

--- app-config.ts
+++ app-config.ts
@@ -1,1 +1,1 @@
-export let CONFIG = { business_username: "juicenblendz", business_id: "193", appId: "0", googleProjectId: "0", marketPlace: false};
+export let CONFIG = { business_username: "glensdinette", business_id: "97", appId: "0", googleProjectId: "0", marketPlace: false};
