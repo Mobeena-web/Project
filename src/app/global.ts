aaa8504d23e4ffd77212bd0a230b03d5e5ba1244
Zohra Awan <zohra@mikronexus.com>
2020-09-07 12:18:22 +0500

    ok

--- src/app/global.ts
+++ src/app/global.ts
@@ -167,8 +167,8 @@ export class GlobalVariable {
     logs = true;
 
     constructor(public alertCtrl: AlertController, public toastCtrl: ToastController) {
-        this.BaseUrl = 'https://staging.onlineordering.mikronexus.com/index.php/';
-        // this.BaseUrl = 'https://onlineordering.mikronexus.com/online-ordering-new/index.php/';
+        // this.BaseUrl = 'https://staging.onlineordering.mikronexus.com/index.php/';
+        this.BaseUrl = 'https://onlineordering.mikronexus.com/online-ordering-new/index.php/';
         // this.BaseUrl = 'http://192.168.100.3/online-ordering-rewamp/index.php/'
     }
 
