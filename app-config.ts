16f558dcbacdb5cbbdf7a31cbcca84242d03e8c5
Danyaal <danyaalmaan@mikronexus.com>
2021-11-03 09:54:35 +0500

    ok

--- app-config.ts
+++ app-config.ts
@@ -1,1 +1,1 @@
-export let CONFIG = { business_username: "parknailspa", business_id: "360", appId: "", googleProjectId: "", marketPlace: false, company: 'mknx', env: 'production' };
+export let CONFIG = { business_username: "mknx", business_id: "76", appId: "", googleProjectId: "", marketPlace: false, company: 'mknx', env: 'production' };
