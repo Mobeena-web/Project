9f9a14991d52bace3e08bed953f39689ef277981
Zohra Awan <zohra@mikronexus.com>
2021-11-15 10:20:01 +0500

    ok

--- app-config.ts
+++ app-config.ts
@@ -1,1 +1,1 @@
-export let CONFIG = { business_username: "juicenblendz", business_id: "193", appId: "", googleProjectId: "", marketPlace: false, company: 'mknx', env: 'production' };
+export let CONFIG = { business_username: "tropicaljerkmain", business_id: "380", appId: "", googleProjectId: "", marketPlace: false, company: 'mknx', env: 'production' };
